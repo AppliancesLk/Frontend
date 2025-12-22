353ebb358eeed9d06245668fa183f6f8956e7529
Buddhikamadumali <buddhikamadumali4@gmail.com>
2025-12-22 09:54:31 +0530

    Update home section UI

--- src/components/home/CategoryGrid.jsx
+++ src/components/home/CategoryGrid.jsx
@@ -1,18 +1,35 @@
+import Feather from "feather-icons-react";
 import { categories } from "../../data/categories";
 
 export default function CategoryGrid() {
   return (
-    <div className="px-6 py-10">
-      <h3 className="text-2xl font-bold mb-4">Categories</h3>
+    <section className=" px-6 py-10">
+      <div className="flex justify-between items-center">
+        <h3 className="text-2xl font-bold text-black">
+          Categories
+        </h3>
+        <button className="cursor-pointer flex items-center">
+          View all
+          <Feather icon="chevron-right" size={16} className="ml-2" />
+        </button>
+      </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-        {categories.map(cat => (
-          <div key={cat.id} className="rounded-md overflow-hidden shadow hover:scale-105 transition">
-            <img src={cat.image} alt={cat.name} className="w-full h-32 object-cover" />
-            <p className="text-center p-3 font-semibold">{cat.name}</p>
+      <div className="flex gap-10 overflow-x-auto scrollbar-hide pb-4 justify-center">
+        {categories.map((cat) => (
+          <div key={cat.id} className="flex flex-col items-center">
+            <div className="my-5 mx-2 w-30 h-30 rounded-full bg-gray-100 flex items-center justify-center overflow-hidden shadow-lg hover:scale-110 transition duration-300 cursor-pointer">
+              <img
+                src={cat.image}
+                alt={"#"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="mt-3 text-center font-semibold text-gray-900 text-sm">
+              {cat.name}
+            </p>
           </div>
         ))}
       </div>
-    </div>
+    </section>
   );
 }
--- src/components/home/ProductListSection.jsx
+++ src/components/home/ProductListSection.jsx
@@ -1,10 +1,19 @@
+import  Feather  from "feather-icons-react";
 import { products } from "../../data/products";
 import ProductCard from "./ProductCard";
 
 export default function ProductListSection() {
   return (
     <div className="px-6 py-10">
-      <h3 className="text-2xl font-bold mb-4">Featured Products</h3>
+       <div className="flex justify-between items-center mb-8">
+        <h3 className="text-2xl font-bold text-black">
+         Featured Products
+        </h3>
+        <button className="cursor-pointer flex items-center">
+          View all
+          <Feather icon="chevron-right" size={16} className="ml-2" />
+        </button>
+      </div>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
         {products.map(prod => (
