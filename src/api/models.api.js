bfafbdad4265e31d1e700b15055dbb9e52efe8f7
lakshithamadushan779988 <lakshithamadushan779988@gmail.com>
2026-03-10 21:59:09 +0530

    edit .env variable name

--- src/api/models.api.js
+++ src/api/models.api.js
@@ -1,6 +1,6 @@
 // api/category.api.js
 import axios from "axios";
-const API_BASE = import.meta.env.VITE_API_BASE_URL; 
+const API_BASE = import.meta.env.REACT_APP_API_BASE_URL; 
 
 
 export const fetchModels = async (brandId) => {
